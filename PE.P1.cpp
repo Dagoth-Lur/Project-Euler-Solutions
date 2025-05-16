b06a9538da8daa559c2ab41f9395b256f1575836
Dagoth-Lur <0pewpews@gmail.com>
2025-05-16 13:37:54 +0100

    Update PE.P1.cpp

--- PE.P1.cpp
+++ PE.P1.cpp
@@ -1,16 +1,1 @@
-#include <iostream>
 
-using namespace std;
-
-int main() {
-  
-    int Count = 0;
-    for (int i = 3; i < 1000; i++) {
-      
-        if (i % 3 == 0 || i % 5 == 0)
-            Count = Count + i;
-    }
-  
-    cout << Count << endl;
-    return 0;
-}
